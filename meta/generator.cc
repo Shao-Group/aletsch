25347521abf355d968d153fd2e7f3d256e400d41
Mingfu Shao <shaomingfu@gmail.com>
2020-03-28 05:56:56 +0000

    test all

--- meta/generator.cc
+++ meta/generator.cc
@@ -65,7 +65,7 @@ int generator::resolve()
 		//ht.print();
 
 		// TODO for test
-		if(ht.tid >= 3) break;
+		//if(ht.tid >= 1) break;
 
 		//if(ht.nh >= 2 && p.qual < min_mapping_quality) continue;
 		//if(ht.nm > max_edit_distance) continue;
