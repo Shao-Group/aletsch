bfe32e2573ebea0a3a431ea1ac61e46889231ae9
Mingfu Shao <shaomingfu@gmail.com>
2020-06-13 21:53:30 +0000

    take max of coverage for single-exon

--- meta/generator.cc
+++ meta/generator.cc
@@ -271,11 +271,6 @@ bool generator::assemble(splice_graph &gr, phase_set &ps, vector<pereads_cluster
 	bool b = build_single_exon_transcript(gr, t);
 	if(b == false) return false;
 
-	if(t.coverage < cfg.min_single_exon_transcript_coverage) return true;
-	if(t.length() < cfg.min_single_exon_transcript_length) return true;
-
-	ts.add(t, 2, sp.sample_id, TRANSCRIPT_COUNT_ADD_COVERAGE_ADD);
-
 	if(cfg.output_bridged_bam_dir != "" && vc.size() >= 1)
 	{
 		sp.open_bridged_bam(cfg.output_bridged_bam_dir);
@@ -287,6 +282,10 @@ bool generator::assemble(splice_graph &gr, phase_set &ps, vector<pereads_cluster
 		sp.close_bridged_bam();
 	}
 
+	//if(t.coverage < cfg.min_single_exon_transcript_coverage) return true;
+	//if(t.length() < cfg.min_single_exon_transcript_length) return true;
+
+	ts.add(t, 2, sp.sample_id, TRANSCRIPT_COUNT_ADD_COVERAGE_ADD);
 	return true;
 }
 
