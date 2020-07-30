0430c9843f5d48003203b164fe0f10510a28cfb6
Mingfu Shao <shaomingfu@gmail.com>
2020-07-30 09:11:44 -0400

    add chain_set

---
+++ rnacore/chain_set.cc
@@ -0,0 +1,77 @@
+/*
+Part of aletsch
+(c) 2020 by Mingfu Shao, The Pennsylvania State University
+See LICENSE for licensing.
+*/
+
+#include "essential.h"
+#include "constants.h"
+#include "chain_set.h"
+
+int chain_set::add(const vector<int32_t> &v, int h)
+{
+	if(v.size() <= 0)
+	{
+		printf("error: adding empty chain with id to chain_set\n", h);
+		return 0;
+	}
+
+	if(hmap.find(h) != h.end()) 
+	{
+		printf("error: id %d has already been added to chain_set\n", h);
+		return 0;
+	}
+
+	int32_t p = v[0];
+	if(pmap.find(p) == pmap.end())
+	{
+		vector<vector<int32_t>> vv;
+		vv.push_back(v);
+		chains.push_back(vv);
+		int n = chains.size() - 1;
+		pmap.insert(make_pair(p, n));
+		hmap.insert(make_pair(h, PI(n, 0)));
+	}
+	else
+	{
+		int k = pmap[p];
+		assert(k >= 0 && k < chains.size());
+		vector<vector<int32_t>> &vv = chains[k];
+		bool found = false;
+		for(int i = 0; i < vv.size(); i++)
+		{
+			if(vv[i] == v)
+			{
+				hmap.insert(make_pair(h, PI(k, i)));
+				found = true;
+				break;
+			}
+		}
+
+		if(found == false)
+		{
+			vv.push_back(v);
+			int n = vv.size() - 1;
+			hmap.insert(make_pair(h, PI(k, n)));
+		}
+	}
+	return 0;
+}
+
+int chain_set::print()
+{
+	map<int, int> m;
+	for(int i = 0; i < chains.size(); i++)
+	{
+		int n = chains[i].size();
+		if(m.find(n) == m.end()) m.insert(make_pair(n, 1));
+		else m[n]++;
+	}
+
+	printf("chain_set: %lu groups, %lu stored hits\n", pmap.size(), hmap.size());
+	for(auto &x : m)
+	{
+		printf("chain_set: %d groups with %d chains\n", x.second, x.first);
+	}
+	return 0;
+}
---
+++ rnacore/chain_set.h
@@ -0,0 +1,30 @@
+/*
+Part of aletsch
+(c) 2020 by Mingfu Shao, The Pennsylvania State University
+See LICENSE for licensing.
+*/
+
+#ifndef __CHAIN_SET_H__
+#define __CHAIN_SET_H__
+
+#include <map>
+#include <set>
+#include <vector>
+
+#include "util.h"
+
+using namespace std;
+
+class chain_set
+{
+public:
+	map<int, PI> hmap;
+	map<int32_t, int> pmap;
+	vector<vector<vector<int32_t>>> chains;
+
+public:
+	int add(const vector<int32_t> &v, int h);
+	int print();
+};
+
+#endif
