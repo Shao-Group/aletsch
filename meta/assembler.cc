4b9a9854f310722e1f357e9213a6daecac1404fb
Mingfu Shao <shaomingfu@gmail.com>
2022-09-11 00:57:10 +0000

    update; just for a test

--- meta/assembler.cc
+++ meta/assembler.cc
@@ -654,9 +654,9 @@ int assembler::assemble(splice_graph &gx, phase_set &px, int sid)
 	}
 	*/
 
-	for(int k = 0; k < 5; k++)
+	for(int k = 0; k < 20; k++)
 	{
-		boost::asio::post(pool, [this, &gx, &hx, k, sid]
+		boost::asio::post(pool, [this, gx, hx, k, sid]
 		{
 			splice_graph gr(gx);
 			hyper_set hs(hx);
